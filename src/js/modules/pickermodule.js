9f056ea459dfc3ae58ed8bed59978155ba9040b4
Michael Brown <brownieboy@gmail.com>
2016-02-06 09:34:50 +1100

    Now working with kendoDatePicker() loaded from an ES6 module by SystemJs alone (no build step)

--- src/js/modules/pickermodule.js
+++ src/js/modules/pickermodule.js
@@ -1,7 +1,11 @@
+import 'kendo.datepicker';
 
 class DatePickerField {
 	constructor(configObj) {
 		console.log("constructor called");
+
+		$("#testpicker").kendoDatePicker({});
+		console.log("date picker called");
 	}
 }
 
